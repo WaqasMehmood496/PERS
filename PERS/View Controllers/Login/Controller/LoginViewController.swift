130462acf5ccc420b85a0b9ed589edcf023a079d
WaqasMehmood100 <malikwaqas100@hotmail.com>
2021-06-09 16:02:17 +0500

    Login Screen

---
+++ PERS/View Controllers/Login/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  PERS
+//
+//  Created by Buzzware Tech on 09/06/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: IBOUTLET'S
+    
+    // MARK: VARIABLE'S
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: ACTION'S
+
+}
